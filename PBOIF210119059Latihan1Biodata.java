4b8e45dcb22fd200ff7522e0c61eaa598c03c298
irgidp <irgijak@gmail.com>
2020-10-15 21:46:24 +0700

    First Commit

---
+++ PBOIF210119059Latihan1Biodata.java
@@ -0,0 +1,30 @@
+/*
+ * @author
+ * NAMA        : Irgi Dwiputra
+ * KELAS       : IF-2
+ * NIM         : 10119059
+ * Deskripsi   : Program ini berisi program untuk menampilkan biodata
+ */
+package pboif2.pkg10119059.latihan1.biodata;
+
+/**
+ *
+ * @author Corazon
+ */
+public class PBOIF210119059Latihan1Biodata {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------");
+        System.out.println("NAMA\t\t: Irgi Dwiputra");
+        System.out.println("ALAMAT\t\t: Jl. Sekeloa No.23B");
+        System.out.println("TEMPAT LAHIR\t: Jakarta");
+        System.out.println("TANGGAL LAHIR\t: 29 April 2002");
+        System.out.println("----------------");
+        System.out.println("HOBI\t\t: Tidur");
+        System.out.println("MAKES\t\t: Yang pake nasi");
+        System.out.println("MIKES\t\t: Air putih");
+    }
+
+    
+}
